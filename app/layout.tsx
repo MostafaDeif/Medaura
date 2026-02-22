826b0c90b16997043918f34781f5578dea15f04c
MostafaDeif <mostafadeif11@gmail.com>
2026-02-22 16:58:53 +0200

    Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Cairo, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/nav/nav";
+import Footer from "@/components/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       >
         <Navbar />
         <div className="lg:px-12 xl:px-24">{children}</div>
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <BestDoctors />
       <BestClinics />
       <WhatClientSay />
-      <Footer />
     </main>
   );
 }
